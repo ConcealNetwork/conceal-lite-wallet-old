aff02f1b03959f6bbd85a9d257a0dc52b3e093d1
Travis Winsor [Kinetic IT] <Travis.Winsor@kineticit.com.au>
2020-02-27 17:34:45 +1100

    add enter animation

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,9 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, transition, query, style, stagger, animate } from '@angular/animations';
 
 @Component({
 	selector: 'app-settings',
 	templateUrl: './settings.component.html',
-	styleUrls: ['./settings.component.scss']
+	styleUrls: ['./settings.component.scss'],
+  animations: [
+    trigger(
+      'enterAnimation', [
+        transition(':enter', [
+          style({opacity:0}),
+          animate(400, style({opacity:1}))
+        ]),
+        transition(':leave', [
+          style({opacity:1}),
+          animate(400, style({opacity:0}))
+        ])
+      ]
+    ),
+    trigger('stagger', [
+      transition('* => *', [
+        query('#cards', style({ opacity: 0, transform: 'translateX(-40px)' }), {optional: true}),
+        query('#cards', stagger('300ms', [
+          animate('600ms 1.2s ease-out', style({ opacity: 1, transform: 'translateX(0)' })),
+        ]), {optional: true}),
+        query('#cards', [
+          animate(1000, style('*'))
+        ], {optional: true})
+      ])
+    ])
+  ]
 })
 export class SettingsComponent implements OnInit {
 
