44e1f27a8d5bb8f10a22631d42e595e49d4d359c
Travis Winsor <travis_winsor@hotmail.com>
2020-04-16 15:54:03 +1000

    add dialog for new contacts

--- src/app/modules/contacts/contacts.component.ts
+++ src/app/modules/contacts/contacts.component.ts
@@ -5,6 +5,8 @@ import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 export interface Contacts {
   label: string;
@@ -48,8 +50,8 @@ const names: string[] = [
     ]),
     trigger('detailExpand', [
       state('collapsed', style({display: 'none'})),
-      state('expanded', style({display: 'show', 'border-bottom-width': 0, height: '60px'})),
-      transition('expanded <=> collapsed', animate(200, style({opacity:0}))),
+      state('expanded', style({display: 'show', height: '60px'})),
+      transition('expanded <=> collapsed', animate(1, style({opacity:0}))),
     ]),
   ]
 })
@@ -59,7 +61,7 @@ export class ContactsComponent implements OnInit {
   pageEvent: PageEvent;
   pageSize: Number = 10;
   pageSizeOptions: number[] = [5, 10, 25, 100];
-  displayedColumns: string[] = ['select', 'label', 'address', 'paymentid'];
+  displayedColumns: string[] = ['select', 'options', 'label', 'address', 'paymentid'];
   dataSource: MatTableDataSource<Contacts>;
   selection = new SelectionModel<Contacts>(true, []);
   isExpansionDetailRow = (i: number, row: Object) => row.hasOwnProperty('detailRow');
@@ -97,7 +99,16 @@ export class ContactsComponent implements OnInit {
     return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.label + 1}`;
   }
 
-	constructor(public dialog: MatDialog) {
+	constructor(
+    public dialog: MatDialog,
+    public matIconRegistry: MatIconRegistry,
+    public domSanitizer: DomSanitizer
+  ) {
+    matIconRegistry.addSvgIconSet(
+      domSanitizer.bypassSecurityTrustResourceUrl(
+        `assets/materal-icons-twotone.svg`
+      )
+    );
     // Create 100 users
     const contacts = Array.from({length: 100}, (_, k) => this.createContacts(k + 1));
     // Assign the data to the data source for the table to render
