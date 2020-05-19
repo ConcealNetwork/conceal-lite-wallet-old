553ac498a4cfde96e85b432510d8aa40c88301df
Travis Winsor <travis_winsor@hotmail.com>
2020-05-19 10:32:16 +1000

    cleanup

--- src/app/modules/settings/settings.component.ts
+++ src/app/modules/settings/settings.component.ts
@@ -1,3 +1,4 @@
+// Import Core
 import { Component, OnInit } from '@angular/core';
 import { trigger, transition, query, style, stagger, animate } from '@angular/animations';
 import { TranslateService } from '@ngx-translate/core';
@@ -6,49 +7,49 @@ import { TranslateService } from '@ngx-translate/core';
 	selector: 'app-settings',
 	templateUrl: './settings.component.html',
 	styleUrls: ['./settings.component.scss'],
-  animations: [
-    trigger(
-      'enterAnimation', [
-        transition(':enter', [
-          style({opacity:0}),
-          animate(400, style({opacity:1}))
-        ]),
-        transition(':leave', [
-          style({opacity:1}),
-          animate(400, style({opacity:0}))
-        ])
-      ]
-    ),
-    trigger('stagger', [
-      transition('* => *', [
-        query('#cards', style({ opacity: 0, transform: 'translateX(-40px)' }), {optional: true}),
-        query('#cards', stagger('300ms', [
-          animate('600ms 1.2s ease-out', style({ opacity: 1, transform: 'translateX(0)' })),
-        ]), {optional: true}),
-        query('#cards', [
-          animate(1000, style('*'))
-        ], {optional: true})
-      ])
-    ])
-  ]
+	animations: [
+		trigger(
+			'enterAnimation', [
+				transition(':enter', [
+					style({opacity:0}),
+					animate(400, style({opacity:1}))
+				]),
+				transition(':leave', [
+					style({opacity:1}),
+					animate(400, style({opacity:0}))
+				])
+			]
+		),
+		trigger('stagger', [
+			transition('* => *', [
+				query('#cards', style({ opacity: 0, transform: 'translateX(-40px)' }), {optional: true}),
+				query('#cards', stagger('300ms', [
+					animate('600ms 1.2s ease-out', style({ opacity: 1, transform: 'translateX(0)' })),
+				]), {optional: true}),
+				query('#cards', [
+					animate(1000, style('*'))
+				], {optional: true})
+			])
+		])
+	]
 })
 export class SettingsComponent implements OnInit {
 
-  isLoading: boolean = true;
-  currentLang: string;
+	isLoading: boolean = true;
+	currentLang: string;
 
-  constructor(private translate: TranslateService) { 
-    this.translate.use('en');
-  }
-  
-  useLanguage(language: string) {
-    this.translate.use(language);
-    this.currentLang = language;
-  }
+	constructor(private translate: TranslateService) { 
+		this.translate.use('en');
+	}
+	
+	useLanguage(language: string) {
+		this.translate.use(language);
+		this.currentLang = language;
+	}
 
 	ngOnInit(): void {
-    this.isLoading = false;
-    this.currentLang = this.translate.currentLang;
+		this.isLoading = false;
+		this.currentLang = this.translate.currentLang;
 	}
 
 }
